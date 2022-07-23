338d84117771d66f2136121868a739362c2ba0fb
landeron <CUBEMIRROR@outlook.com>
2022-07-22 21:56:50 -0600

    finish the introduccion

--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,SafeAreaView ,ScrollView,Platform } from 'react-native';
+import {dogs} from './data';
+//si no instalas la package cagas
+
 /*
 function Card3(props){ // esta forma seria la normal 
   return (
@@ -22,29 +25,48 @@ const Card2 = ({text}) => <Text> {text} </Text> ; //arrow function directo
 const Card = (props) => { 
 
   const {name, desc, image, width, height} = props;
+
   return (
-    <View>
-      <Text>{name}</Text>
+    <View style={{ width: 350, height: 526, backgroundColor: 'white',padding:10,borderRadius:30,marginBottom:30  }}>
+      <Image 
+        style={{ width: '100%', height: 420, borderRadius: 20, backgroundColor: 'orange' }}
+        source ={{uri: image}}
+       />
+      <Text style={{ fontSize: 28, fontWeight: 'bold' }}>{name}</Text>
+      <Text style={{ fontSize: 20,color:'gray' }}>{desc}</Text>
     </View>
   )
 }
 
-
+//tuve varios errores con imprimir un texto solucion:
+// tiene que meterse en un array
+// no meter comentarios dentro del codigo
+// cerrar bien los espacios 
+//puedo usar el nombre de la propiedad sin usar el name del array 
 export default function App() { //componente frame,layout main el container, se puee usar solo el componente
   return (
-    <View style={styles.container}> 
-
-      <Card name={"riquin"}/>
-
-    </View>
+    <SafeAreaView style={styles.container}> 
+      <ScrollView contentContainerStyle={{marginHorizontal:18}} Style ={{ marginTop:Platform.OS === 'android' && '30' }}>
+      {dogs.map( (dog,index)=> {
+        
+        return(
+          <Card key={index}{...dog} />
+        )
+      })}
+      <Card name={"Pinguino que tomo las fotos"}/>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: (200, 200, 200),
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+/*
+{dogs.map( ({name, desc, image})=> {index, dog array
+*/
---
+++ mobile/data.js
@@ -0,0 +1,17 @@
+export const dogs = [
+    {
+        name: "Riquin",
+        desc: "asynckit is asynckit is asynckit is asyn",
+        image: "https://i.imgur.com/tn7EHiJ.png"
+    },
+    {
+        name: "Juan",
+        desc: "asd  is  the         name of the project that    will be used to communicate",
+        image: "https://i.imgur.com/gtGjn4u.png"
+    },
+    {
+        name: "Jose Luis",
+        desc: "descriptors formatted    ",
+        image: "https://i.imgur.com/Qd5dcJS.png"
+    },
+]
