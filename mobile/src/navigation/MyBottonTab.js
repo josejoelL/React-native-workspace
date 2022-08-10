4aa339264ad0813b6b1e3546aeb3181098ac096f
landeron <CUBEMIRROR@outlook.com>
2022-08-10 15:32:24 -0600

    add icons to tabnav

--- mobile/src/navigation/MyBottonTab.js
+++ mobile/src/navigation/MyBottonTab.js
@@ -5,37 +5,73 @@ import Search from "../screens/Search";
 import Reels from "../screens/Reels";
 import Settings from "../screens/Settings";
 import { Octicons } from '@expo/vector-icons';
+import { Colors } from "../constants/color";
 const MyTab = createBottomTabNavigator();
 
 export default function MyBottomTab () {
     return (
 
-        <MyTab.Navigator initialRouteName="Home">
+        <MyTab.Navigator initialRouteName="Home"
+            screenOptions={{
+                tabBarActiveTintColor: Colors.dark,
+                //tabBarInactiveBackgroundColor: "black",
+               // tabBarActiveBackgroundColor: "black",
+             
+               
+               
+            }}>
+                
                 <MyTab.Screen
                     name   = "Home"
                     component={Home}
                     
                     options={{
                         tabBarShowLabel: false,
-                        tabBarIcon:({color})=> <Octicons name="home" size={24} color="black" />
+                       
+                        tabBarIcon:({color})=> <Octicons name="home" size={24} color={color} />
                     }}
                     
                 />
                 <MyTab.Screen
                     name="Search"
                     component={Search}
+
+
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ color }) => <Octicons name="book" size={24} color={color} />
+                }}
                 />
+
                 <MyTab.Screen
                     name="Reels"
                     component={Reels}
+
+
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ color }) => <Octicons name="sun" size={24} color={color} />
+                }}
                 />
                 <MyTab.Screen
                     name="Settings"
                     component={Settings}
+
+
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ color }) => <Octicons name="apps" size={24} color={color} />
+                }}
                 />
                 <MyTab.Screen
                     name="Profile"
                     component={Profile}
+
+
+                options={{
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ color }) => <Octicons name="circle" size={24} color={color} />
+                }}  
                 />
             </MyTab.Navigator>
 
