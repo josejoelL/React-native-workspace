3e917835e3672e6761bca58bdedfe7f1ec9af136
landeron <CUBEMIRROR@outlook.com>
2022-08-15 21:27:57 -0600

    add character in data

--- mobile/src/screens/Youkoso/character/data.js
+++ mobile/src/screens/Youkoso/character/data.js
@@ -5,5 +5,77 @@ export const pfps = [
         desc: "Profesora de la clase D primer año",
         image: "https://i.imgur.com/txi8RZY.png"
     },
-   
+    {
+        name: "HorikitaManabu",
+        nick: "Horikita Manabu",
+        desc: "Ex presidente del consejo estudiantil del 3er año",
+        image: " "
+    },
+    {
+        name: "IkeKanji",
+        nick: "Kanji",
+        desc: "1-D",
+        image: " "
+    },
+    {
+        name: "MiyakeAkito",
+        nick: "Akito",
+        desc: "1-D",
+        image: " "
+    },
+    {
+        name: "NagumoMiyabi",
+        nick: "Nagumo",
+        desc: "1-D",
+        image: " "
+    },
+    {
+        name: "SudouKen",
+        nick: "Sudou",
+        desc: "1-D",
+        image: " "
+    },
+    {
+        name: "YamauchiHaruki",
+        nick: "Yamauchi",
+        desc: "1-D",
+        image: " "
+    },
+    {
+        name: "YukimuraTeruhiko",
+        nick: "Yukimura",
+        desc: "1-D",
+        image: " "
+    },
+    {
+        name: "KatsuragiKouhei",
+        nick: "Katsuragi",
+        desc: "1-A",
+        image: " "
+    }, {
+        name: "AyanokoujiKiyotaka",
+        nick: "Ayanokouji",
+        desc: "1-D",
+        image: " "
+    }, {
+        name: "KanzakiRyuuji",
+        nick: "Kanzaki",
+        desc: "1-B",
+        image: " "
+    }, {
+        name: "RyuuenKakeru",
+        nick: "Ryuuen",
+        desc: "1-D",
+        image: " "
+    }, {
+        name: "HirataYousuke",
+        nick: "Hirata",
+        desc: "1-D",
+        image: " "
+    }, {
+        name: "KouenjiRokusuke",
+        nick: "Yukimura",
+        desc: "1-D",
+        image: " "
+    },
 ]
