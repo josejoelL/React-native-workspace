35904e18bb80bfb01b16567960c3d5660e60f3aa
landeron <CUBEMIRROR@outlook.com>
2022-08-21 23:09:08 -0600

    ikn how to use an api

--- mobile/src/screens/Settings.js
+++ mobile/src/screens/Settings.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function Settings() {
     let uri ='https://animechan.vercel.app/api/random'
-    fetch(uri).then(response => response.json()).then(quote => {
-            
-            //console.log(quote)
-            console.log(quote.anime);
+
+    const res = fetch(uri).then(response => response.json()).then(quote => {
+           console.log(quote.anime);
             console.log(quote.character);
             console.log(quote.quote);
-        });
 
+        });
    return (
         <View style={globalStyles.ScreenContainer}>
             <Text style={globalStyles.title}> a </Text>
