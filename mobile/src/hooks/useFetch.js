406bea82e1ea634962550cda381aac5d495da033
landeron <CUBEMIRROR@outlook.com>
2022-08-24 22:25:27 -0600

    add useFetch code

---
+++ mobile/src/hooks/useFetch.js
@@ -0,0 +1,62 @@
+import { useEffect, useReducer, useState } from "react";
+
+
+    /*
+    const [state, setState] = useState();
+    const [error, setError] = useState();
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    */
+   
+
+   function reducer(state, {type, ResponseJSON, error}) { 
+    switch (type) {
+            case 'loading':
+                return {...state, isLoading: true};
+
+            case "success":
+                return {
+                    
+                    ResponseJSON,
+                    error: error,
+                    isLoading: false,
+                };
+            case    "error": 
+            return {ResponseJSON:null, isLoading: false, error: error};
+            case 'default': 
+                throw new Error("Uknown action type");
+            }        
+   }   
+export function useFetch (url) {
+    
+        const [state, dispatch] = useReducer(reducer,{
+            ResponseJSON: null,
+            isLoading: true,
+            error: null,
+
+        });
+    useEffect(() =>{
+
+        const callFetch = async = () => {
+                let shouldCancel = false;
+
+                dispatch({type: 'loading'});
+                try {
+                    const response = await fetch(url);
+
+                    const ResponseJSON = await response.json();
+                    if (shouldCancel) return;
+                    dispatch({type: 'success',ResponseJSON});
+
+                }catch(error) {
+                    if (shouldCancel) return;
+                    dispatch({type: 'error',error});
+                }
+
+            }
+        callFetch();
+        return () => (shouldCancel = true);
+
+    },  [])
+    return state;
+}
