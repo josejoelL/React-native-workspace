ab1388744fac6c67f2e27e4ee7b6fe6a51ee2afd
landeron <CUBEMIRROR@outlook.com>
2022-08-25 22:33:38 -0600

    learn how to use de object

--- mobile/src/screens/Settings.js
+++ mobile/src/screens/Settings.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,FlatList } from 'react-native';
 import { globalStyles } from '../styles/global';
 import {useFetch} from '../hooks/useFetch';
 import { T } from '../constants/Texts';
@@ -7,22 +7,37 @@ import { T } from '../constants/Texts';
     let urlNaruto = "https://animechan.vercel.app/api/quotes/anime?title=naruto"
 // import useSRW from 'srw'; me falta yarn add swr y ver la parte del add
 //const { data, error } = useSRW(url, fetch(url).then(response => response.json()));
+//
     
 export default function Settings() {
-  const {isLoading, error, ResponseJSON} = useFetch(url);
-    console.log({ResponseJSON});
+    const { isLoading, error, ResponseJSON } = useFetch(url);
+    //console.log({ResponseJSON});
+   let animechan = JSON.parse(JSON.stringify(ResponseJSON))
+   console.log( "anime : "+animechan.anime );
+    console.log("personaje : " +animechan.character);
     if (isLoading) return <T e='Loading...' />;
     if (error) return <T e='wait in other hours...' />;
+
    return (
-        <View style={globalStyles.ScreenContainer}>
-            <Text > {JSON.stringify(ResponseJSON)} </Text> 
-
-
+        <View style={globalStyles.container}>
+        <Text style={globalStyles.title}> {animechan.anime} </Text>
+        <Text style={globalStyles.title}> {animechan.character} </Text>
+       <Text style={globalStyles.title}> {animechan.quote } </Text>
+       
         </View>
     );
 }
 
-/*
+/*  
+
+ <FlatList
+           data={responseJSON.anime}
+           renderItem={({ item }) => <Text>{item} </Text>}
+           keyExtractor={item => item.character }
+        />
+
+
+
   React.useEffect(() => {
         fetch(uri)
         .then(res => res.json())
