146fedc2ab667b3bf561726af035a735224583b9
landeron <CUBEMIRROR@outlook.com>
2022-08-26 23:49:32 -0600

    adding array of images for naruto characters

---
+++ mobile/src/constants/NarutoCharData.js
@@ -0,0 +1,17 @@
+export const characters = [
+    {
+        name: "Naruto Uzumaki",
+        desc: " ",
+        image: " "
+    },
+    {
+        name: "Sakura Haruno",
+        desc: " ",
+        image: " "
+    },
+    {
+        name: "Sasuke Uchiha",
+        desc: " ",
+        image: " "
+    },
+]
